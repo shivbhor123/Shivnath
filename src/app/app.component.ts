bf881040ddc043c33aac4b72983d0776cca7a87e
shivnath <bhorshivnath21@gmail.com>
2024-02-18 14:30:21 +0530

    add project file

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavbarComponent, AboutComponent, LoginComponent]
+})
+export class AppComponent {
+  title = 'project_3';
+}
